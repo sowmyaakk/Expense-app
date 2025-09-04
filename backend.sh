0885a6c2f8b4cc24764c0bda477b88f66a8f6adf
Sowmya <akkina.sowmya@capgemini.com>
2025-09-04 15:57:51 +0530

    auto push

--- backend.sh
+++ backend.sh
@@ -12,7 +12,7 @@ npm install
 chmod -R 775 /app
 chown -R expense:expense /app
 dnf install mysql-server -y
-mysql -h 172.31.34.99 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.39.58 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 systemctl daemon-reload
 systemctl enable backend 
 systemctl start backend 
